77ae361e44f2a6e2bfc14b070550e015efcd2faf
Roman-Efremov <roma-ju@tut.by>
2019-12-30 18:28:32 +0300

    express

---
+++ Christmas-tree-express/express/routes/adress.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const router = express.Router();
+const faker = require('faker');
+
+/* GET home page. */
+router.get('/', (req, res) => {
+   var adress = [ 
+       { "city": faker.address.city(), 
+  	   	 "street": faker.address.streetAddress()
+  	   }
+  	];
+});
+module.exports = router;
